5e0a889127d1659b5156868cf7d9792d91e684a9
Thomas Lux <thomas.ch.lux@gmail.com>
2019-09-26 16:44:50 -0500

    Updated setup script for testing.

--- balltree/__init__.py
+++ balltree/__init__.py
@@ -3,6 +3,7 @@ import os
 
 DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 ABOUT_DIR = os.path.join(DIRECTORY, "about")
-__version__ = open(os.path.join(ABOUT_DIR,"version.txt")).read().strip()
+with open(os.path.join(ABOUT_DIR,"version.txt")) as f:
+    __version__ = f.read().strip()
 
 from .balltree import *
