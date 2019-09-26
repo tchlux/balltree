6138f29e91409a83d5492a2fdf8590b4111ba8b5
Thomas Lux <thomas.ch.lux@gmail.com>
2019-09-26 16:47:05 -0500

    Updated setup script for testing.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
         download_url = 'https://github.com/{git_username}/{package}/archive/{version}.tar.gz'.format(
             git_username=git_username, package=package, version=version),
         description = description,
-        scripts=[os.path.join(package,package,"setup.py")],
+        scripts=[os.path.join(package,"setup.py")],
         keywords = keywords,
         python_requires = '>=3.6',
         license='MIT',
