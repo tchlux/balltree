86f89e6e233841f37acfbb39760db5f11a7ab2d2
Thomas Lux <thomas.ch.lux@gmail.com>
2019-09-26 17:07:34 -0500

    Updated setup script for testing.

--- balltree/setup.py
+++ balltree/setup.py
@@ -3,22 +3,3 @@
 # Any python code that should be executed (as if __main__) during
 # setup should go here to prepare the module on a new computer.
 
-print()
-print("RUNNING SETUP SCRIPT NOW!")
-print()
-
-import os
-
-PATH_TO_HERE   = os.path.dirname(os.path.abspath(__file__))
-# Import the Fortran utilities.
-PATH_TO_BT     = os.path.join(PATH_TO_HERE, "ball_tree.f90")
-ball_tree = fmodpy.fimport(PATH_TO_BT, output_directory=PATH_TO_HERE,
-                           autocompile_extra_files=True)
-PATH_TO_SORT   = os.path.join(PATH_TO_HERE, "fast_sort.f90")
-fast_sort = fmodpy.fimport(PATH_TO_SORT, output_directory=PATH_TO_HERE,
-                           autocompile_extra_files=True)
-PATH_TO_SELECT = os.path.join(PATH_TO_HERE, "fast_select.f90")
-fast_select = fmodpy.fimport(PATH_TO_SELECT, output_directory=PATH_TO_HERE,
-                             autocompile_extra_files=True)
-
-
